cf61c41ca585e8dcf265997977e5ef9dffd2e0fb
cwhite911 <smortopahri@gmail.com>
2014-11-19 16:54:02 -0500

    Added documentation to new directive

--- app/scripts/directives/documentForm.js
+++ app/scripts/directives/documentForm.js
@@ -7,13 +7,14 @@ angular.module('asbuiltsApp')
       transclude: true,
       scope: {},
       templateUrl: 'views/document-form.html',
-      link: function ($scope, element, attr) {
+      link: function (scope, element, attr) {
+        //Gets correct REST endpoints form ArcGIS server
         var s = ags.testServer.getService().$promise.then(function(res){
            var layers = new ags.AgsLayers(res.layers.concat(res.tables));
            console.log(layers);
         });
-        //Setup Boolean option for utilies options
-        $scope.selectionOptions = {
+        //Setup Boolean option for utilies options..could/should switch to service or provider
+        scope.selectionOptions = {
           bool: [{'name': true, 'id': 1}, {'name': false, 'id': 0}],
         };
       }
