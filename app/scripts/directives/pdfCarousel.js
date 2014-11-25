361bdf798d9beed934df764a8314582b6afb548c
cwhite911 <smortopahri@gmail.com>
2014-11-25 17:07:32 -0500

    Starting to add buttons to carousel

--- app/scripts/directives/pdfCarousel.js
+++ app/scripts/directives/pdfCarousel.js
@@ -12,7 +12,11 @@ angular.module('asbuiltsApp')
       link: function (scope, element, attr) {
           // scope.docs = docs;
           scope.$watchCollection('docs', function(){
-            console.log(scope.docs);
+          });
+          var navBoxLeft = angular.element('.nav-box-left');
+          console.log(navBoxLeft);
+          navBoxLeft.on('click', function(){
+            angular.element(this).toggleClass('nav-arrow1');
           });
       }
     }
