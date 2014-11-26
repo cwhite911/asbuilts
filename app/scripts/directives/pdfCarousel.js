b101a99400befea4e86bd75a49a56e800453336d
cwhite911 <smortopahri@gmail.com>
2014-11-26 11:48:49 -0500

    Added navigation contols to carousel

--- app/scripts/directives/pdfCarousel.js
+++ app/scripts/directives/pdfCarousel.js
@@ -12,12 +12,22 @@ angular.module('asbuiltsApp')
       link: function (scope, element, attr) {
           // scope.docs = docs;
           scope.$watchCollection('docs', function(){
+            scope.moveFoward = function (){
+              var first = scope.docs.shift();
+              scope.docs.push(first);
+            }
+            scope.moveBackward = function (){
+              var last = scope.docs.pop();
+              scope.docs.unshift(last);
+            }
           });
-          var navBoxLeft = angular.element('.nav-box-left');
-          console.log(navBoxLeft);
-          navBoxLeft.on('click', function(){
-            angular.element(this).toggleClass('nav-arrow1');
-          });
+          // var navBoxLeft = angular.element('.nav-box-left');
+          // console.log(navBoxLeft);
+          // navBoxLeft.on('click', function(){
+          //   angular.element(this).toggleClass('nav-arrow1');
+          // });
+
+
       }
     }
 });
