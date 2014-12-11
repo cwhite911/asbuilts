a2521416b73464d82d6e1ef5a9ec6a834a0be740
cwhite911 <smortopahri@gmail.com>
2014-12-11 13:31:51 -0500

    Starting to stylize recent project list

--- app/scripts/controllers/recent.js
+++ app/scripts/controllers/recent.js
@@ -4,6 +4,7 @@
 angular.module('asbuiltsApp')
   .controller('RecentCtrl', ['$scope','$cookieStore', '$rootScope',
     function ($scope, $cookieStore, $rootScope) {
+      //Gets root scope so that the change in project will trigger the watch and update $scope.recent
       var scope = $rootScope;
         $scope.recent = $cookieStore.get('projects');
         $scope.$watchCollection("myrecent", function(newVal, oldVal){
