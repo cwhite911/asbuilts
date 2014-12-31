a366fa39b6497fe7813d4326f6801508f8d0bbf9
Corey White <smortopahri@gmail.com>
2014-12-31 16:23:36 -0500

    Working on renaming middleware for file uploads

--- server/app.js
+++ server/app.js
@@ -2,13 +2,20 @@
 
 var express = require('express'),
     app= express(),
-    upload = require('./routes/upload')
+    logger = require('morgan'),
+    upload = require('./routes/upload');
 
+//Set port
 var port = process.env.PORT || 9080;
 
+//Add middleware
+app.use(logger('combined'));
 app.use(express.static(__dirname + '/public'));
+
+//Add Routes
 app.use('/upload', upload);
 
+//Set server to listen to port at 9080
 app.listen(port, function(){
   console.log('Server is listening on port ' + port + '\n');
 });
--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,20 +1,31 @@
 'use strict';
 
 var express = require('express'),
-    logger = require('morgan'),
-    multer  = require('multer'),
-    router = express.Router();
+    router = express.Router(),
+    bodyParser = require('body-parser'),
+    multer  = require('multer');
 
-router.use(logger('combined'));
+router.use(bodyParser.json()); // for parsing application/json
+router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+router.use(multer({
+  dest: './public/documents',
+  rename: function (fieldname, filename){
+    console.log(fieldname);
+    console.log(filename);
+  }
+}));
 
-router.route('/upload')
+
+router.route('/')
   .get(function(req, res){
-    console.log("I posted");
+    console.log("I GET it...");
     res.send('GET requests are not accepted');
   })
   .post(function(req, res){
-    console.log("I posted");
-    res.send('Post successful');
+    console.log(req.body);
+    console.log(req.files);
+    console.log(req.get('Content-Type'));
+    res.json(req.body);
   });
 
 
