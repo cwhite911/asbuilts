6887eafbecd535308e93045ec480d0f2d119ed78
cwhite911 <smortopahri@gmail.com>
2015-02-03 16:48:52 -0500

    Added get max projectid to create new feature

--- app/scripts/directives/mapEdit.js
+++ app/scripts/directives/mapEdit.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('asbuiltsApp')
-.directive('mapEdit', ['Ags', '$filter', function (Ags, $filter) {
+.directive('mapEdit', ['Ags', '$rootScope', function (Ags, $rootScope) {
   return {
     restrict: 'E',
     transclude: true,
@@ -12,11 +12,43 @@ angular.module('asbuiltsApp')
     templateUrl: 'views/map-edit.html',
     link: function (scope, element) {
       //Gets correct REST endpoints form ArcGIS server
-      var url;
+      $rootScope.pt_fs = $rootScope.mapstest.setService({
+        folder:'PublicUtility',
+        service: 'ProjectTracking',
+        server: 'FeatureServer'
+      });
+      var options = {
+        actions: 'query',
+        layer: 'Project Tracking',
+        params: {
+          f: 'json',
+          outFields: 'PROJECTID',
+          orderByFields: 'PROJECTID DESC',
+          returnGeometry: false,
+          where: 'PROJECTID IS NOT NULL',
+          // groupByFieldsForStatistics: 'PROJECTID',
+          // outStatistics: {
+          //     "statisticType": "max",
+          //     "onStatisticField": "PROJECTID",
+          //     "outStatisticFieldName": "maxid"
+          // }
+
+        }
+
+      };
       scope.$watchCollection('active', function(oldVal, newVal){
         console.log(scope.active);
         if (scope.active){
           angular.element('.angular-leaflet-map').addClass('map-move-left');
+          $rootScope.pt_fs.request(options).then(function(data){
+            scope.currentMaxProjectId = data.features[0].attributes.PROJECTID;
+            scope.newMaxProjectId = scope.currentMaxProjectId + 1;
+            console.log(scope.currentMaxProjectId);
+            console.log(scope.newMaxProjectId);
+          },
+          function (err){
+            console.log(err);
+          }); //.request()
         }
         else {
           angular.element('.angular-leaflet-map').removeClass('map-move-left');
