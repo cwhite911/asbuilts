fc2cbc9ca5902ea93655d691b82b9be198d7f871
cwhite911 <smortopahri@gmail.com>
2015-02-20 13:55:23 -0500

    Added server local strat

--- server/app.js
+++ server/app.js
@@ -3,19 +3,58 @@
 var express = require('express'),
     app= express(),
     logger = require('morgan'),
+    path = require('path'),
+    // favicon = require('serve-favicon'),
+    cookieParser = require('cookie-parser'),
+    session = require('cookie-session'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    login = require('./routes/login'),
     upload = require('./routes/upload');
 
 //Set port
-var port = process.env.PORT || 9080;
-
+app.set('port', process.env.PORT || 8000);
 //Add middleware
-app.use(logger('combined'));
-app.use(express.static(__dirname + '/public'));
+app.use(logger('common'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({ keys: ['secretkey1', 'secretkey2', '...']}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Configure passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// requires the model with Passport-Local Mongoose plugged in
+var User = require('./models/user');
+
+// use static authenticate method of model in LocalStrategy
+passport.use(new LocalStrategy(User.authenticate()));
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //Add Routes
+// app.use('/register', login);
+// app.use('/login', login);
+// app.use('/logout', login);
+app.use('/', require('./routes/login'));
 app.use('/upload', upload);
 
-//Set server to listen to port at 9080
-app.listen(port, function(){
-  console.log('Server is listening on port ' + port + '\n');
+// mongoose
+mongoose.connect('mongodb://localhost/passport_local_mongoose', function(err) {
+  if (err) {
+    console.log('Could not connect to mongodb on localhost. Ensure that you have mongodb running on localhost and mongodb accepts connections on standard ports!');
+  }
+});
+
+
+
+//Set server to listen to port at 9000
+app.listen(app.get('port'), function(){
+  console.log('Server is listening on port ' + app.get('port') + '\n');
 });
---
+++ server/routes/login.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var passport = require('passport'),
+    Account = require('./../models/user'),
+    bodyParser = require('body-parser'),
+    router = require('express').Router();
+
+// router.get('/', function(req, res) {
+//   res.render('index', { user: req.user });
+// });
+
+//Enable CORS
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+router.use(bodyParser.json()); // for parsing application/json
+router.use(bodyParser.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
+
+router.get('/register', function(req, res) {
+  res.send('Hello');
+  // res.render('register', {});
+});
+
+router.post('/register', function(req, res, next) {
+  console.log('registering user');
+  console.log(req.body);
+  Account.register(new Account({ username: req.body.username }), req.body.password, function(err) {
+    if (err) { console.log('error while user register!', err); return next(err); }
+
+    console.log('user registered!');
+
+    res.redirect('/');
+  });
+});
+
+router.get('/login', function(req, res) {
+  res.send(req.user);
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+  res.redirect('/');
+});
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+module.exports = router;
