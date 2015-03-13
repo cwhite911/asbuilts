57b82fb7c44450c859d73db0226d38f51fb2f8aa
cwhite911 <smortopahri@gmail.com>
2015-03-13 15:49:25 -0400

    Updated addDoc controller with serverFactory

--- app/scripts/controllers/addDoc.js
+++ app/scripts/controllers/addDoc.js
@@ -8,17 +8,13 @@
  * Controller of the asbuiltsApp
  */
 angular.module('asbuiltsApp')
-  .controller('addDocCtrl', ['$scope','CookieService', 'OptionsFactory', 'ags', 'projectSearch', '$rootScope','Ags',
-    function ($scope, CookieService, OptionsFactory, ags, projectSearch, $rootScope, Ags) {
+  .controller('addDocCtrl', ['$scope','CookieService', 'serverFactory', 'projectSearch', '$rootScope',
+    function ($scope, CookieService, serverFactory, projectSearch, $rootScope) {
       //Set root scope as scope
       var scope = $rootScope;
 
       $scope.project = {};
-      var options = new OptionsFactory('json', '*', '', 'DOCID ASC', false );
-      var s = ags.testServer.getService().$promise.then(function(res){
-         var layers = new ags.AgsLayers(res.tables).getLayerId('RPUD.PTK_DOCUMENTS');
-         options.addOptions('id', layers);
-      });
+
       $scope.autoFillProjects = function (typed) {
         //Turns on the map resulsts table
         $scope.searchStatus = false;
@@ -36,9 +32,7 @@ angular.module('asbuiltsApp')
                     $scope.projects.push(data.features[i].attributes.PROJECTNAME + ':' + data.features[i].attributes.DEVPLANID + ':' + data.features[i].attributes.PROJECTID);
                   }
               }
-              // $scope.projects = projectSearch.getSet($scope.projects);
-
-
+          
           }, function (error){
             console.log(error);
         });
@@ -53,9 +47,19 @@ angular.module('asbuiltsApp')
         //Add projects to recent projects cookie
         CookieService.addProjectCookie(typed);
         //Set up GET request options
-        var param = 'PROJECTID = ' + typed.split(':')[2];
-        options.updateOptions('where', param);
-        ags.features.getAll(options).$promise.then(function(data){
+
+        var options = {
+          layer: 'RPUD.PTK_DOCUMENTS',
+          actions: 'query',
+          params: {
+            f: 'json',
+            where: 'PROJECTID = ' + typed.split(':')[2],
+            outFields: '*',
+            orderByFields: 'DOCID ASC',
+            returnGeometry: false
+          }
+        };
+        serverFactory.pt_fs.request(options).then(function(data){
 
           //If no project documents have been added
           if (!data.features.length){
@@ -76,11 +80,6 @@ angular.module('asbuiltsApp')
           $scope.searchStatus = true;
           $scope.project_docs = true;
         });
-        // $scope.$watchCollection("project", function(newVal, oldVal){
-        //   console.log("The project data changed");
-        //   ags.features.getAll(options).$promise.then(function(data){
-        //     $scope.project = data.features;
-        //   });
-        // });
+
       }
     }]);
