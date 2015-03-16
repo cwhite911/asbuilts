b15a46e68673f50d238540bd2abcf6efda21358b
cwhite911 <smortopahri@gmail.com>
2015-03-16 11:20:44 -0400

    Just rebuilt project in dist

--- server/public/dist/scripts/controllers/head.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('asbuiltsApp')
-  .controller('HeaderController', ['$scope', '$location', function ($scope, $location) {
-    $scope.isActive = function (viewLocation) {
-        return viewLocation === $location.path();
-    };
-}]);
